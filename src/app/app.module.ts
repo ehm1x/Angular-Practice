8ce8c2ac5908cd86660623106406e553d3213f7a
Bryce <bryce6425@gmail.com>
2023-11-30 13:05:11 -0500

    Refactor app.module.ts to remove TestCompComponent and add HomeComponent and LeagueSelectionComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TestCompComponent } from './test-comp/test-comp.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-import { HomePageComponent } from './home-page/home-page.component';
+import { HomeComponent } from './home-page/home-page.component';
+import { LeagueSelectionComponent } from './league-selection/league-selection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestCompComponent,
     HeaderComponent,
-    HomePageComponent
+    HomeComponent,
+    LeagueSelectionComponent
   ],
   imports: [
     BrowserModule,
