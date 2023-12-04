88f6d3adc4e757a56442857750a0b52f87aca584
Bryce <bryce6425@gmail.com>
2023-12-04 16:48:23 -0500

    Blank trade analyzer page for future

---
+++ src/pages/trade-analyzer/trade-analyzer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-trade-analyzer',
+  templateUrl: './trade-analyzer.component.html',
+  styleUrl: './trade-analyzer.component.css'
+})
+export class TradeAnalyzerComponent {
+
+}
