664f0fd277eb7d938a9fc608a62fbfc1b0133d3d
Bryce <bryce6425@gmail.com>
2023-12-04 16:43:56 -0500

    Fix routing and add material theme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../pages/home-page/home-page.component';
+import { PlayerRankingsComponent } from '../pages/player-rankings/player-rankings.component';
+import { TradeAnalyzerComponent } from '../pages/trade-analyzer/trade-analyzer.component';
+import { TeamRankingsComponent } from '../pages/team-rankings/team-rankings.component';
+import { ShowRostersComponent } from '../pages/show-rosters/show-rosters.component';
+import { RostersGuard } from './rosters.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'player-rankings', component: PlayerRankingsComponent, canActivate: [RostersGuard]},
+  {path: 'trade-analyzer', component: TradeAnalyzerComponent, canActivate: [RostersGuard]},
+  {path: 'team-rankings', component: TeamRankingsComponent, canActivate: [RostersGuard]},
+  {path: 'show-rosters', component: ShowRostersComponent, canActivate: [RostersGuard]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,38 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home-page/home-page.component';
-import { LeagueSelectionComponent } from './league-selection/league-selection.component';
+import { HomeComponent } from '../pages/home-page/home-page.component';
+import { LeagueSelectionComponent } from '../pages/home-page/components/league-selection/league-selection.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PlayerRankingsComponent } from '../pages/player-rankings/player-rankings.component';
+import { TeamRankingsComponent } from '../pages/team-rankings/team-rankings.component';
+import { TradeAnalyzerComponent } from '../pages/trade-analyzer/trade-analyzer.component';
+import { ShowRostersComponent } from '../pages/show-rosters/show-rosters.component';
+import { TeamComponent } from '../pages/show-rosters/components/team/team.component';
+import { PlayerComponent } from '../pages/show-rosters/components/player/player.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AvatarComponent } from './avatar/avatar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    LeagueSelectionComponent
+    LeagueSelectionComponent,
+    PlayerRankingsComponent,
+    TeamRankingsComponent,
+    TradeAnalyzerComponent,
+    ShowRostersComponent,
+    TeamComponent,
+    PlayerComponent,
+    AvatarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
