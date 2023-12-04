ad64911aec660863baf72d763f29dc78cf86105d
Bryce <bryce6425@gmail.com>
2023-12-04 16:46:45 -0500

    Moved Player Rankings

---
+++ src/pages/player-rankings/player-rankings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-rankings',
+  templateUrl: './player-rankings.component.html',
+  styleUrl: './player-rankings.component.css'
+})
+export class PlayerRankingsComponent {
+
+}
