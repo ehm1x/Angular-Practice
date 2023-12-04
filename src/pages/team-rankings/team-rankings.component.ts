07228f6e4626503ea451fa0c0af4fdecdc3175a4
Bryce <bryce6425@gmail.com>
2023-12-04 16:48:07 -0500

    Blank team-rankings page for future

---
+++ src/pages/team-rankings/team-rankings.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-team-rankings',
+  templateUrl: './team-rankings.component.html',
+  styleUrl: './team-rankings.component.css'
+})
+export class TeamRankingsComponent {
+
+}
