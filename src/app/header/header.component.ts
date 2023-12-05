603b06834f9d314f693828a8d54af6f5bf658814
Bryce <bryce6425@gmail.com>
2023-12-05 16:07:57 -0500

    Refactor header component styles and HTML, added player Logo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { UserService } from '../services/user-service.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  avatarId$: Observable<string | undefined>; 
+  
 
+  constructor(public userService: UserService) {
+    this.avatarId$ = userService.userData$;
+   }
+ 
 }
