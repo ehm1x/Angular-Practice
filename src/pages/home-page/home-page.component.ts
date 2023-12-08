d22880dccccc5c0752ce69bd8a382152cadd7027
Bryce <bryce6425@gmail.com>
2023-12-08 15:07:11 -0500

    added loading page

--- src/pages/home-page/home-page.component.ts
+++ src/pages/home-page/home-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { LeagueService } from '../../app/services/league.service';
 import { UserService } from '../../app/services/user-service.service';
 import { Observable } from 'rxjs';
@@ -9,17 +9,31 @@ import { League } from '../../app/Interfaces/league';
   selector: 'app-home-page',
   template: `
     <div class="container">
-      <div class="home" *ngIf="username$ | async as username; else welcomeBlock">
-      <h2 style="text-align:center">Welcome, {{ username }}</h2>
-        <ng-container *ngIf="confirmedLeague$ | async as confirmedLeague; else leagueSelection">
-          <div class="mainMenu" *ngIf="confirmedLeague">
+      <div
+        class="home"
+        *ngIf="username$ | async as username; else welcomeBlock"
+      >
+        <h2 style="text-align:center">Welcome, {{ username }}</h2>
+        <ng-container
+          *ngIf="
+            confirmedLeague$ | async as confirmedLeague;
+            else leagueSelection
+          "
+        >
+          <div *ngIf="confirmedLeague">
+          <div class="mainMenu" *ngIf="(this.leagueService.loadProgress$ | async)">
             <h2>Current League : {{ (selectedLeague$ | async)?.name }}</h2>
             <nav>
               <div class="flex">
                 <a routerLink="/show-rosters">Show Rosters</a>
                 <a routerLink="/trade-analyzer">Trade Analyzer</a>
+                <a (click)="handleChangeLeague()">Change League</a>
               </div>
             </nav>
+              </div>
+            <mat-progress-bar class="bar" mode="query" *ngIf="(this.leagueService.loadProgress$ | async) === 0 || null || undefined">
+            </mat-progress-bar>
+
           </div>
         </ng-container>
         <ng-template #leagueSelection>
@@ -29,11 +43,15 @@ import { League } from '../../app/Interfaces/league';
       <ng-template #welcomeBlock>
         <h1 style="padding-bottom: 5px">Welcome</h1>
         <div class="flex">
-          <input type="text" [(ngModel)]="inputUsername" class="username-input" placeholder="Username" />
+          <input
+            type="text"
+            [(ngModel)]="inputUsername"
+            class="username-input"
+            placeholder="Username"
+          />
           <button (click)="handleConfirmUser()">Confirm</button>
         </div>
-        <main>
-        </main>
+        <main></main>
       </ng-template>
     </div>
   `,
@@ -48,7 +66,7 @@ export class HomeComponent {
 
   constructor(
     private userService: UserService,
-    private leagueService: LeagueService
+    public leagueService: LeagueService
   ) {
     this.username$ = userService.username$;
     this.confirmedLeague$ = leagueService.confirmedLeague$;
@@ -57,5 +75,8 @@ export class HomeComponent {
 
   handleConfirmUser() {
     this.userService.setUsername(this.inputUsername);
+  }
+  handleChangeLeague() {
+    this.leagueService.changeLeague();
   }
 }
